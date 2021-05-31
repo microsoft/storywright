294715fdb1dffbbc82f988c44276f8085bc0bf85
Sunil Surana <susurana@microsoft.com>
2021-05-31 21:49:37 +0530

    Refactoring changes

--- src/StoryWright/StoryWright.ts
+++ src/StoryWright/StoryWright.ts
@@ -1,8 +1,12 @@
 import { createElement, useEffect } from 'react';
 import { BrowserExecutor } from './BrowserExecutor';
 
+/**
+ * Wrapper react component
+ * @param p 
+ * @returns 
+ */
 export const StoryWright = p => {
-
   useEffect(() => {
     BrowserExecutor.executesteps(p.steps);
   }, []);
--- src/StoryWrightProcessor/BrowserUtils.ts
+++ src/StoryWrightProcessor/BrowserUtils.ts
@@ -1,10 +1,16 @@
 import * as playwright from 'playwright';
 import { Browser } from './Constants';
 export class BrowserUtils {
+    /**
+     * Returns browser instance for given browser name
+     * @param browserName Name of browser - firefox,chromium
+     * @param headless Whether to start in headless mode
+     * @returns Playwright browser instance
+     */
     public static async getBrowserInstance(browserName: string, headless: boolean) {
         switch (browserName) {
             case Browser.Chromium:
-                return await playwright.chromium.launch({ headless: headless});
+                return await playwright.chromium.launch({ headless: headless });
             case Browser.Firefox:
                 return await playwright.firefox.launch({ headless: headless });
         }
