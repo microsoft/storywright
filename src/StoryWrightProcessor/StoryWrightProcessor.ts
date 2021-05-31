08827205cc3a71d12dfc2a1fc9c6cd2f4e87b72e
Sunil Surana <susurana@microsoft.com>
2021-05-31 16:37:19 +0530

    Open pages as tabs instead of new instance

--- src/StoryWrightProcessor/StoryWrightProcessor.ts
+++ src/StoryWrightProcessor/StoryWrightProcessor.ts
@@ -4,14 +4,23 @@ import { BrowserUtils } from './BrowserUtils';
 import exposePlaywright from './playwrightMethods';
 import { StoryWrightOptions } from './StoryWrightOptions'
 
+/**
+ * Class containing StoryWright operations
+ */
 export class StoryWrightProcessor {
+  /**
+   * 
+   * @param options Storywright processing options
+   */
   public static async process(options: StoryWrightOptions) {
     try {
       const browsers: string[] = options.browsers;
       for (const browserName of browsers) {
         console.log(browserName);
         const browser: Browser = await BrowserUtils.getBrowserInstance(browserName, options.headless);
-        const page: Page = await browser.newPage();
+        const context = await browser.newContext();
+        const page: Page = await context.newPage();
+
         await page.goto(join(options.url, 'iframe.html'));
         const stories: object[] = await page.evaluate(
           '(__STORYBOOK_CLIENT_API__?.raw() || []).map(e => ({id: e.id, kind: e.kind, name: e.name}))'
@@ -38,7 +47,7 @@ export class StoryWrightProcessor {
               }
               try {
                 console.log('async');
-                const page = await browser.newPage();
+                const page = await context.newPage();
                 await page.setViewportSize({
                   width: 1920,
                   height: 964,
