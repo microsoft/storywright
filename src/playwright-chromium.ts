64d7b6df17540130c8ff50d5cabfab8128d4036f
Sunil Surana <susurana@microsoft.com>
2022-10-28 14:31:42 +0530

    update

--- src/playwright-chromium.ts
+++ src/playwright-chromium.ts
@@ -1,5 +1,6 @@
 import { Page, chromium } from "playwright";
 import { join } from "path";
+const fs = require('fs');
 
 async function main(): Promise<void> {
     try {
@@ -7,19 +8,43 @@ async function main(): Promise<void> {
         const browser = await chromium.launch({ args: ["--allow-file-access-from-files"], headless });
         const context = await browser.newContext();
         const page: Page = await context.newPage();
-        await page.goto(join("file:///C:/GIT/repoissue/storybook", `iframe.html?id=avatar-converged--size-active-badge`));
-        await page.waitForTimeout(2000);
+        const pagePath = join(`file:///${process.cwd()}/storybook`, `iframe.html?id=avatar-converged--size-active-badge`);
+        await page.goto(pagePath);
+        await page.waitForTimeout(4000);
         
         let selector: string = ".testWrapper";
         let element = await page.$(selector);
         if (await element.isVisible()) {
-            let screenshotPath = "playwright-chrome.png";
+            let screenshotName = "playwright-chrome-run";
+            let count = 1;
+            while( fs.existsSync(`${screenshotName}_${count}_A.png`))
+            {
+                count++;
+            }
             await element.screenshot({
-                path: screenshotPath,
+                path: `${screenshotName}_${count}_A.png`,
             });
         } else {
             console.log("ERROR: Element NOT VISIBLE: CAPTURING PAGE");
         }
+
+
+        await page.waitForTimeout(2000);
+
+        if (await element.isVisible()) {
+            let screenshotName = "playwright-chrome-run";
+            let count = 1;
+            while( fs.existsSync(`${screenshotName}_${count}_B.png`))
+            {
+                count++;
+            }
+            await element.screenshot({
+                path: `${screenshotName}_${count}_B.png`,
+            });
+        } else {
+            console.log("ERROR: Element NOT VISIBLE: CAPTURING PAGE");
+        }
+
         await page.close();
         await context.close();
         await browser.close();
