95cf83861ee79ddc9d5629d2d50deeec747735fa
Sunil Surana <susurana@microsoft.com>
2022-12-01 13:27:13 +0530

    webkit

---
+++ src/playwright-webkit.ts
@@ -0,0 +1,56 @@
+import { Page, webkit } from "playwright";
+import { join } from "path";
+const fs = require('fs');
+
+async function main(): Promise<void> {
+    try {
+        const headless = true;
+        const browser = await webkit.launch({ headless });
+        const context = await browser.newContext();
+        const page: Page = await context.newPage();
+        const pagePath = join(`file:///${process.cwd()}/storybook`, `iframe.html?id=avatar-converged--size-active-badge`);
+        await page.goto(pagePath);
+        await page.waitForTimeout(4000);
+        
+        let selector: string = ".testWrapper";
+        let element = await page.$(selector);
+        if (await element.isVisible()) {
+            let screenshotName = "playwright-webkit-run";
+            let count = 1;
+            while( fs.existsSync(`${screenshotName}_${count}_A.png`))
+            {
+                count++;
+            }
+            await element.screenshot({
+                path: `${screenshotName}_${count}_A.png`,
+            });
+        } else {
+            console.log("ERROR: Element NOT VISIBLE: CAPTURING PAGE");
+        }
+
+
+        await page.waitForTimeout(2000);
+
+        if (await element.isVisible()) {
+            let screenshotName = "playwright-webkit-run";
+            let count = 1;
+            while( fs.existsSync(`${screenshotName}_${count}_B.png`))
+            {
+                count++;
+            }
+            await element.screenshot({
+                path: `${screenshotName}_${count}_B.png`,
+            });
+        } else {
+            console.log("ERROR: Element NOT VISIBLE: CAPTURING PAGE");
+        }
+
+        await page.close();
+        await context.close();
+        await browser.close();
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+main();
